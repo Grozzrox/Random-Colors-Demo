17ee336478c906282e5ea3588dd88e3c6e9b867c
Andrew <grozzy09@gmail.com>
2020-12-12 15:38:57 -0500

    12Dec2020_1

--- app.js
+++ app.js
@@ -15,3 +15,19 @@ setInterval(function() {
         letter.style.color = randomRGB();
     }
 }, 1000);
+
+setInterval(function() {
+    const h1 = document.querySelector('h1');
+    h1.classList.toggle('small');
+}, 3000)
+
+function brownBaths() {
+    const btn = document.querySelector('button');
+    if (btn.innerText === 'Shop store') {
+        btn.innerText = 'I crave brown baths';
+    } else {
+        btn.innerText = 'Shop store';
+    }
+}
+
+// const newButon = document.createElement('button');
